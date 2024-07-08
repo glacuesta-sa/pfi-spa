9b1da4611f0173e906f178174ec8c53604d378c3
NachoRondan <nacho@Windows11-Desktop>
2024-07-08 18:10:44 -0300

    refactor: removed warnings and bad imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// import { useEffect, useState } from "react";
+// import type { Schema } from "../amplify/data/resource";
+// import { generateClient } from "aws-amplify/data";
 import CustomAppBar from "./components/Appbar";
 import Dendogram from "./components/Dendogram";
 import TreeMap from "./components/TreeMap";
 
-const client = generateClient<Schema>();
+// const client = generateClient<Schema>();
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  // const [nodes, setNodes] = useState<Array<Schema["DendogramNode"]["type"]>>([]);
 
-  useEffect(() => {
-    client.models.DendogramNode.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  // useEffect(() => {
+  //   client.models.DendogramNode.observeQuery().subscribe({
+  //     next: (data) => setNodes([...data.items]),
+  //   });
+  // }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+
 
   return (
     <main>
